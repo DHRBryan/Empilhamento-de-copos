c196ddba9c8eb854fdde7ef7e9d2c81d85b9a1c6
DHRBryan <42739367+DHRBryan@users.noreply.github.com>
2019-11-14 19:59:39 -0300

    Empilhamento de copos

--- Pilha/src/app/App.java
+++ Pilha/src/app/App.java
@@ -3,21 +3,22 @@ import jdk.nashorn.internal.parser.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int numeroDeCopos, coposColocados, numeroFileiras;
+        int numeroDeCopos, coposColocados, numeroFileiras = 0;
+        Pilha torre = new Pilha();
         java.util.Scanner leitor = new Scanner(System.in);
+
         System.out.println("Bem vindo ao app de empilhar copos!");
         System.out.println("-----------------------------------");
         System.out.println("Digite a quantidade de copos que deseja empilhar (Mínimo = 1, Máximo = 26)");
         numeroDeCopos = leitor.nextInt();
-        coposColocados = numeroFileiras = 0;
-        int coposUltimaFileira = numeroDeCopos;              //Representa a quantidade digitada pelo usuário decrementando os copos já colocados
+
         while(coposColocados < numeroDeCopos){
-            aux -= coposColocados;
-            numeroFileiras++;
-            coposColocados += numeroFileiras;
+            String copo = "|__|";
+            torre.empilhar(novoCopo);
+            coposColocados++;                    //Empilha objetos de copos um por na torre
         }
-        for(int i = 0; i < numeroFileiras; i++){
-            System.out.println("Fileira:" + numeroFileiras + "" + numeroFileiras);
-        } 
+        while(!torre.pilhaVazia()){
+            
+        }
     }
 }

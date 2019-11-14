44aea2a5233be67e7192f9f706a8791bfc858c7a
DHRBryan <42739367+DHRBryan@users.noreply.github.com>
2019-11-14 19:23:49 -0300

    Tentativas inúteis de infrutíferas

--- Pilha/src/app/App.java
+++ Pilha/src/app/App.java
@@ -1,7 +1,23 @@
 package app;
+import jdk.nashorn.internal.parser.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello darkness my old friend");
+        int numeroDeCopos, coposColocados, numeroFileiras;
+        java.util.Scanner leitor = new Scanner(System.in);
+        System.out.println("Bem vindo ao app de empilhar copos!");
+        System.out.println("-----------------------------------");
+        System.out.println("Digite a quantidade de copos que deseja empilhar (Mínimo = 1, Máximo = 26)");
+        numeroDeCopos = leitor.nextInt();
+        coposColocados = numeroFileiras = 0;
+        int coposUltimaFileira = numeroDeCopos;              //Representa a quantidade digitada pelo usuário decrementando os copos já colocados
+        while(coposColocados < numeroDeCopos){
+            aux -= coposColocados;
+            numeroFileiras++;
+            coposColocados += numeroFileiras;
+        }
+        for(int i = 0; i < numeroFileiras; i++){
+            System.out.println("Fileira:" + numeroFileiras + "" + numeroFileiras);
+        } 
     }
 }
